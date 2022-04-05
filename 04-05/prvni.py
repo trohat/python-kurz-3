497edd027170a67d2d566124dffff84bfb0fb47e
Trohat <petr.sed@centrum.cz>
2022-04-05 20:24:37 +0200

    První lekce

---
+++ 04-05/prvni.py
@@ -0,0 +1,18 @@
+print("ahoj")
+print("tohle je můj první program")
+print("Teď budeme zadávat čísla...")
+
+cislo = input("Zadej číslo:")
+cislo = int(cislo)
+
+#tohle je komentář - pouze jednořádkový
+#víceřádkové komentáře v Pythonu nejsou :)
+cislo2 = int(input("Zadej jiné číslo:"))
+
+print("První číslo je", cislo, "a druhé číslo je", cislo2)
+
+vysledek = cislo + cislo2
+
+print("Součet čísel je: ", end="")
+print(vysledek)
+
